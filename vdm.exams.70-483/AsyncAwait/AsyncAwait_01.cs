b3cd71d102a0abbcf4342016791732c6c554132e
vdm <dutchpowercow@gmail.com>
2015-03-14 14:34:17 +0100

    Async await

---
+++ vdm.exams.70-483/AsyncAwait/AsyncAwait_01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace vdm.exams.cs.AsyncWait
+{
+    class AsyncAwait_01
+    {
+        static void RunMe()
+        {
+            string result = DownloadContent().Result;
+            Console.WriteLine(result);
+        }
+
+        static async Task<string> DownloadContent()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("http://www.microsoft.com"); // return task of string to caller when finished
+                return result;
+            }
+        }
+    }
+}
---
+++ vdm.exams.70-483/AsyncAwait/AsyncAwait_02.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+
+namespace vdm.exams.cs.AsyncWait
+{
+    class AsyncAwait_02
+    {
+        static void RunMe()
+        {
+            // this example shows responsiveness vs performance. SleepAsyncA method uses a thread from the threadpool while sleeping. The second method however does not occupy
+            // a thread while waiting for the timer to run. The second method gives you scalability.
+        }
+
+        static Task SleepAsyncA(int msTimeout)
+        {
+            return Task.Run(() => Thread.Sleep(msTimeout));
+        }
+
+        static Task SleepAsyncB(int msTimeout)
+        {
+            TaskCompletionSource<bool> tcs = null;
+            var t = new Timer(delegate { tcs.TrySetResult(true); }, null, -1, -1);
+            tcs = new TaskCompletionSource<bool>(t);
+            t.Change(msTimeout, -1);
+            return tcs.Task;
+        }
+    }
+}
